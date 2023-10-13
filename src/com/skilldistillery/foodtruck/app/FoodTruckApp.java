815480462cb3c84859a108b2670a8b7fb13cc201
diego.delapuente35 <diego.a.delapuente@gmail.com>
2023-10-13 15:20:42 -0700

    this is the initial commit

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,23 @@
+package com.skilldistillery.foodtruck.app;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
+public class FoodTruckApp {
+
+	public static void main(String[] args) {
+		int maxFleetSize = 5;
+		private FoodTruck[] fleet = new FoodTruck[5];
+		
+		
+		FoodTruckApp fta = new FoodTruckApp();
+		fta.doYourThing();
+	}
+		
+
+	public void doYourThing() {
+		//prompt user
+		//write out a menu for user input
+		//yada yada yada
+	}
+
+}
---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class FoodTruck {
+	private static int nextTruckId;
+	int numbericId;
+	String truckName;
+	String foodType;
+	int rating;
+	
+
+}
